07e9fd6b283aa3794887393797f968c1b8eb375e
Ubuntu <ubuntu@ip-172-31-43-197.us-west-2.compute.internal>
2016-11-23 16:38:38 +0000

    add image generation capability

--- oneira-api.go
+++ oneira-api.go
@@ -2,10 +2,15 @@ package main
 
 import (
   "fmt"
+  "bytes"
+  "encoding/base64"
+  "os"
+  "html/template"
+  "os/exec"
   "net/http"
+  "image/png"
   "strings"
   "log"
-  "os/exec"
 )
 
 func test(w http.ResponseWriter, r *http.Request){
@@ -18,7 +23,33 @@ func test(w http.ResponseWriter, r *http.Request){
 }
 
 func generate(w http.ResponseWriter, r *http.Request){
-  //code to generate images and shit
+    generated := exec.Command("th", "../oneira_art/main.lua -i ~/dcgan_vae_torch/checkpoints_for_prod/save_cpu_model.lua -o generations/" )
+    if _, err := generated.Output(); err != nil {
+        fmt.Fprint(w, "Error. Please try again.")
+    } else {
+        if img, err = os.Open("../oneira_art/generations/generation.png"); err != nil {
+            log.Println("unable to open file")
+        }
+        defer img.Close()
+        
+        var ImageTemplate string = `<DOCTYPE html>
+            <html lang="en"><head></head>
+<body><img src="data:image/jpg;base64,{{.Image}}"></body>`
+        
+        buffer := new(bytes.Buffer)
+        if err := png.Encode(buffer, *img); err != nil {
+            log.Println("unable to encode image.")
+        }
+        str := base64.StdEncoding.EncodeToString(buffer.Bytes())
+        if tmpl, err := template.New("image").Parse(ImageTemplate); err != nil {
+            log.Println("unable to parse image template")
+        } else {
+            data := map[string]interface{}{"Image": str}
+            if err = templ.Execute(w, data); err != nil {
+                log.Println("unable to execute template.")
+            }
+        }
+    }
 }
 
 func sayHelloFoucault(w http.ResponseWriter, r *http.Request){
